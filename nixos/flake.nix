a9df0d1a60570264996bb827649c0133db6a2897
atyrode <alex.tyrode@outlook.fr>
2024-03-31 19:19:15 +0200

    still figuring out flakes

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,2 +1,18 @@
 {
+    description = "Flake of atyrode";
+
+    inputs = { 
+        nixpkgs.url = "nixpkgs/nixos-23.11";
+    };
+
+    outputs = { self, nixpkgs, ... }:
+    let lib = nixpkgs.lib;
+    in {
+        nixosConfigurations = {
+            alex-pc = lib.nixosSystem {
+                system = "x86_64-linux";
+                modules = [ ./alex-pc.nix ];
+            };
+        };
+    };
 }
