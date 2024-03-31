6aecf0588177f6643cae8565604d4b316f2f1fd2
atyrode <alex.tyrode@outlook.fr>
2024-03-31 18:55:37 +0200

    let there be light

---
+++ nixos/nixosModules/hyprland.nix
@@ -0,0 +1,50 @@
+{ config, pkgs, lib, ... }: {
+
+    security.polkit.enable = true;
+    security.rtkit.enable = true;
+
+    programs.hyprland = {
+        enable = true;
+        enableNvidiaPatches = true;
+        xwayland.enable = true;
+    };
+
+    environment.sessionVariables = {
+        WLR_NO_HARDWARE_CURSORS = "1";
+        NIXOS_OZONE_WL = "1";
+    };
+
+    hardware = {
+        opengl.enable = true;
+        nvidia.modesetting.enable = true;
+    };
+
+    services.xserver = {
+        layout = "fr";
+        xkbVariant = "";
+    };
+
+    console.keyMap = "fr";
+
+    environment.systemPackages = with pkgs; [
+        kitty
+        rofi-wayland
+        waybar
+        swww
+        dunst
+        libnotify
+        lxqt.lxqt-policykit
+    ];
+
+    xdg.portal.enable = true;
+    xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+    sound.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        jack.enable = true;
+    };
+}
